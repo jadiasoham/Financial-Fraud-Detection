1afb38ddddcef12bffc00c6d431564727b2d5470
Soham Jadia <jadiasoham@gmail.com>
2024-05-27 00:41:55 +0530

    ..

--- trial.py
+++ trial.py
@@ -1,9 +1,6 @@
 from sklearn.ensemble import IsolationForest
-from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
-import seaborn as sns
-sns.set_theme('notebook')
 
 file_path = r'datasets\Fraud Analytics Dataset.xlsx'
 data = pd.read_excel(file_path)
